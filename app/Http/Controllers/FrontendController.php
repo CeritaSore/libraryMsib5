5b5f47c2ab3dd17859780bb17abe3eaa70f43098
Fajar Septianto <fajar23.septianto@gmail.com>
2023-12-26 16:35:28 +0700

    update view, nambah seeder, dan nambah migrasi

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Buku;
+use App\Models\Kategori;
+use App\Models\Team;
 use Illuminate\Http\Request;
 
 class FrontendController extends Controller
@@ -10,21 +13,31 @@ class FrontendController extends Controller
     public function index()
     {
         $title = 'home';
-        return view('frontend.home', compact('title'));
+        $listbuku = Buku::all();
+        return view('frontend.home', compact('title', 'listbuku'));
     }
-    public function feature() {
+    public function feature()
+    {
+        $listbuku = Buku::all();
+
         $title = 'feature';
-        return view('frontend.feature', compact('title'));
-        
+        return view('frontend.feature', compact('title', 'listbuku'));
     }
-    public function popular() {
+    public function popular()
+    {
+        $listbuku = Buku::all();
+        $kat1 = Buku::where('kategori_id', 1)->get();
+        $kat2 = Buku::where('kategori_id', 2)->get();
+        $kat3 = Buku::where('kategori_id', 3)->get();
+        $kat4 = Buku::where('kategori_id', 4)->get();
+
         $title = 'popular';
-        return view('frontend.pop', compact('title'));
-        
+        return view('frontend.pop', compact('title','listbuku','kat1','kat2','kat3','kat4'));
     }
-    public function about() {
+    public function about()
+    {
         $title = 'about';
-        return view('frontend.about', compact('title'));
-        
+        $listTeam = Team::all();
+        return view('frontend.about', compact('title','listTeam'));
     }
 }
